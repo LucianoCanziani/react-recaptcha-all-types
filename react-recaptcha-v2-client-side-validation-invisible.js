00378c2cc3687cffe666943502fd9c7ae0c4de5c
Luciano Canziani <96893895+LucianoCanziani@users.noreply.github.com>
2022-10-15 22:43:16 -0300

    Add files via upload

---
+++ react-recaptcha-v2-client-side-validation-invisible.js
@@ -0,0 +1,35 @@
+import React, { useState, useRef } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+
+const App = () => {
+
+  const captcha = useRef();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const token = await captcha.current.executeAsync();
+    if (token) {
+      alert("The data has been sent!");
+    } else {
+      alert(
+        "You were not able to verify your humanity :("
+      );
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" />
+        <ReCAPTCHA
+          ref={captcha}
+          sitekey="Here you add your reCaptcha API key"
+          size="invisible"
+          type="image"// or audio test
+        />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
+};
+export default App;
